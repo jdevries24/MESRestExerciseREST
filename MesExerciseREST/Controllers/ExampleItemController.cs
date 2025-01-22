49a5738913e17fdf70d731099ddaa2fc3c6ec485
Jeffrey DeVries <jeffdevriesrocks@gmail.com>
2025-01-22 10:43:39 -0500

    Init Commit. Frame worked different calls for the API.

---
+++ MesExerciseREST/Controllers/ExampleItemController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MesExerciseREST.Controllers
+{
+    [ApiController]
+    [Route("apiv1")]
+    public class ExampleItemController : Controller
+    {
+        [Route("GetItemByKey/{key}")]
+        [HttpGet]
+        public ActionResult<ExampleItem> GetItemByKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("SerchItemsByValue/{value}")]
+        [HttpGet]
+        public ActionResult<IEnumerable<ExampleItem>> SearchItemsByValue(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("NewItem")]
+        public ActionResult<ExampleItem> NewItem(ExampleItem NewItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("UpdateItem")]
+        public ActionResult<ExampleItem> UpdateItem(ExampleItem ItemToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("DeleteItem/{key}")]
+        [HttpDelete]
+        public ActionResult DeleteItem(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MesExerciseREST/ExampleItem.cs
@@ -0,0 +1,8 @@
+﻿namespace MesExerciseREST
+{
+    public class ExampleItem
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
