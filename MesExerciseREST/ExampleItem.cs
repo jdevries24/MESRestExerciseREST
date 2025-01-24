6cdf25e4d63ccf3b0aa023cdce9110fadb4c9da7
Jeffrey DeVries <jeffdevriesrocks@gmail.com>
2025-01-24 11:27:53 -0500

    Forgot to add Key Anotation for DB item

--- MesExerciseREST/ExampleItem.cs
+++ MesExerciseREST/ExampleItem.cs
@@ -1,7 +1,9 @@
-﻿namespace MesExerciseREST
+﻿using System.ComponentModel.DataAnnotations;
+namespace MesExerciseREST
 {
     public class ExampleItem
     {
+        [Key]
         public string Key { get; set; }
         public string Value { get; set; }
     }
