c51d8cec1f8f6f185dabccffdf2b81bdf90129c0
Jeffrey DeVries <jeffdevriesrocks@gmail.com>
2025-01-24 11:28:31 -0500

    Initalize Interfaces for DB controller

--- MesExerciseREST/Program.cs
+++ MesExerciseREST/Program.cs
@@ -1,7 +1,9 @@
+using MesExerciseREST.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<EntitiyFrameworkInterface>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
