97d2a7069d07b0e2d74156285b678120f780d53b
Jeffrey DeVries <jeffdevriesrocks@gmail.com>
2025-01-24 10:37:22 -0500

    Added first Three methods to the interface. No Error checking yet. Simple methods to ensure that DB is working Correctly.

--- MesExerciseREST/Interfaces/ExampleItemInterface.cs
+++ MesExerciseREST/Interfaces/ExampleItemInterface.cs
@@ -8,5 +8,20 @@
             _context = new AplicationDBContext();
             _context.Database.EnsureCreated();
         }
+
+        public bool DoesItemExist(string Key)
+        {
+            return _context.ItemTable.Where(item => item.Key == Key).Any();
+        }
+
+        public ExampleItem GetItem(string Key)
+        {
+            return _context.ItemTable.Where(item => item.Key == Key).First();
+        }
+
+        public void AddItem(ExampleItem newItem)
+        {
+            _context.ItemTable.Add(newItem);
+        }
     }
 }
