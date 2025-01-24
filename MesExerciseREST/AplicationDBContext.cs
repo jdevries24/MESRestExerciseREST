b2082f80dbb7e2011bb4828bcb634ee7cabe978b
Jeffrey DeVries <jeffdevriesrocks@gmail.com>
2025-01-24 10:20:44 -0500

    Added DB Context for project. Will use In memory DB for debugging/Development purposes

---
+++ MesExerciseREST/AplicationDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MesExerciseREST
+{
+    public class AplicationDBContext : DbContext
+    {
+        public DbSet<ExampleItem> ItemTable { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseInMemoryDatabase("ApplicationDB.db");
+    }
+}
